538748ca6621aea6c0a388f746838611a73efad0
tamicristina <tami.cristina06@gmail.com>
2021-06-04 21:45:59 -0300

    tabuleiro do jogo finalizado, iniciando funções do java script

---
+++ main.js
@@ -0,0 +1,9 @@
+const containerJogo = document.querySelector(".tabuleiro");
+
+const imgX = document.createElement("img");
+const imgO = documento.createElement("img");
+
+imgX.setAttribute("src", "./imagens/X.png");
+imgX.setAttribute("class", "imgx");
+
+console.log(containerJogo);
