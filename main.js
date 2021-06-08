1f2a3645bf5918b2d3ac94f24a448b133e2a59e9
tamicristina <tami.cristina06@gmail.com>
2021-06-08 18:44:28 -0300

    Desenvolvendo função para a lógica do jogo

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const containerJogo = document.querySelector(".tabuleiro");
-const containerEspaco = document.querySelector(".container");
+//const containerJogo = document.querySelector(".tabuleiro");
+//const containerEspaco = document.querySelector(".container");
+//Pegando as div do HTML
 let espacos = document.querySelectorAll(
   ".espaço1,.espaço2,.espaço,.espaço4,.espaço5,.espaço6,.espaço7,.espaço8,.espaço9"
 );
-// console.log(espacos);
 
 function jogar() {
   let jogador = true;
@@ -12,55 +12,51 @@ function jogar() {
       if (espaco.innerHTML === "") {
         if (jogador) {
           const imgO = document.createElement("img");
+          espaco.setAttribute("id", "imgo");
           imgO.setAttribute("class", "imgo");
           imgO.src = "./imagens/02.png";
           espaco.appendChild(imgO);
-
-          console.log(espaco);
           jogador = false;
+          jogo();
         } else if (!jogador) {
           const imgX = document.createElement("img");
+          espaco.setAttribute("id", "imgx");
           imgX.setAttribute("class", "imgx");
           imgX.src = "./imagens/X.png";
           espaco.appendChild(imgX);
-          console.log(espaco);
           jogador = true;
+          jogo();
         }
       }
     });
   }
 }
 
+function jogo() {
+  let jogadorO = document.getElementById("imgo");
+  let jogadorX = document.getElementById("imgx");
+
+  for (var i = 0; i < espacos.length; i++) {
+    var o = [];
+    if (jogadorO == espacos[i]) {
+      //aqui vou colocar um objeto pra receber uma key recebendo o tipo da img e o value com o index da posição
+      //pra teste
+
+      console.log(espacos[i]); // vai ser a key
+      console.log(i); //vai ser o value
+      console.log("esse  é O");
+    } else if (testx == espacos[i]) {
+      console.log(espacos[i]);
+      console.log(i);
+      console.log("Aquie é o X");
+    }
+  }
+  // if (jogadorO == espacos[0]) {
+  //   console.log("Ok");
+  // } else if (espacos[2] === jogadorX) {
+  //   console.log("não foi");
+  // }
+  // console.log(espacos[0]);
+}
+
 jogar();
-
-// for (let espaco of espacos) {
-//     console.log(espaco);
-//     espaco.addEventListener("click", function () {
-//       if (contador % 2 === 0) {
-//         espaco.appendChild(imgO);
-//       } else if (contador % 2 == 1) {
-//         espaco.appendChild(imgX);
-//         contador++;
-//       }
-//     });
-
-// for (let i = 0; i < espacos.length; i++) {
-//   espacos.addEventListener("click", function () {
-//     espacos.appendChild(imgO);
-//   });
-//   // espacos.addEventListener("click", function () {
-//   //   espacos.appendChild(imgX);
-//   // });
-
-//   console.log(espacos[i]);
-// if (contador % 2 === 0) {
-//   espacos[i].addEventListener("click", function () {
-//     espacos[contador].append(imgO);
-//     contador++;
-//   });
-// } else if (contador % 2 === 1) {
-//   espacos[i].addEventListener("click", function () {
-//     espacos[contador].append(imgX);
-//     contador++;
-//   });
-// }
